5691da1cfa5a1b5e4977e5296715aacb791d6c69
K.McMurray <biofart@pm.me>
2024-06-21 14:06:39 -0400

    added disk_usage

---
+++ disk-usage-script/disk_usage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set terminal title
+echo -ne "\033]0;Disk Usage\007"
+
+clear
+echo "13ah0xxx disk usage...."
+
+# Set terminal geometry
+wmctrl -r :ACTIVE: -e 0,0,80,650,200
+
+# Run ncdu in the terminal
+sudo ncdu /
+
+read -rp "Press Enter to close..."
